0d360735bf75c2eb020695a0a2c0a9fa880ecf69
ZeenatHaseer <99154311+ZeenatHaseer@users.noreply.github.com>
2022-04-23 21:02:15 -0400

    Anagram created

---
+++ src/TC001/TC002.java
@@ -0,0 +1,15 @@
+package TC001;
+
+public class TC002 {
+    public static void main(String[] args) {
+       String a = "java";
+       String b = "vagja";
+        for (int i = 0; i <a.length() ; i++) {
+            char letter = a.charAt(i);
+            b = b.replaceFirst(letter+"","");
+
+        }
+        System.out.println(b.isEmpty()?"Yes":"No");
+
+    }
+}
